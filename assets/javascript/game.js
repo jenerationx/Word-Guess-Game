6142ab143296dfb3e7a1002c70afe4cdf0bef47b
jenerationx <jenerationx74@gmail.com>
2019-02-09 23:11:51 -0500

    this is all i got

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,50 @@
 var possibleWords = ["shame", "dragon", "mountain", "daenerys", "lannister", "throne", "dothraki", "direwolf", "littlefinger", "longclaw", "baratheon", "targaryen", "wildling", "whitewalker", "hodor", "winterfell"];
 var totalWins = 0;
 var lettersGuessed = "";
-var activeWord = "";
 var remainingGuesses = 8;
 var progressWord = [];
 var resetLetters = "";
-var AllChars = [];
 var gameOver = false;
-for (var i=97; i<123; i++)
-    AllChars.push(String.fromCharCode(i));
     
-document.onkeyup = function(event) {
+//reset page to a new word and clear letters guessed
+function resetPage() {
+lettersGuessed = [];
+progressWord = [];
 
-// Wins: (# of times user guessed the word correctly).
-function wins () {
-    document.getElementById("wins-total").innerHTML = totalWins++;
-};
-console.log(totalWins)
+
 // If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
-var randomWord = possibleWords[Math.floor(Math.random() * (possibleWords.length))];
-
-for (i = 0; i < randomWord.length; i++) {
-    progressWord.push("_");
+randomWord = possibleWords[Math.floor(Math.random() * (possibleWords.length))];
+    for (i = 0; i < randomWord.length; i++) {
+        progressWord.push("_");
+    };
+    console.log(randomWord)
+    
 };
-document.getElementById("current-word").innerHTML = progressWord.join(" ");
 
-console.log(randomWord)
-}
-// // As the user guesses the correct letters, reveal them: m a d o _  _ a.
-// // if correct guess, display in randomWord
-//     //else, put into the alreadyGuessed div in an appended? array
+function startGame() {
+// Wins: (# of times user guessed the word correctly).
+    document.getElementById("wins-total").innerHTML = totalWins;
+    console.log(totalWins)
+    // Current progress of word/letters guessed
+    var progressWordText = "";
+    for (i = 0; i < progressWord.length; i++) {
+        progressWordText = progressWordText + progressWord[i];
+    };
+    document.getElementById("current-word").innerHTML = progressWordText;
+    document.getElementById("already-guessed").innerHTML = lettersGuessed;
+    // // Number of Guesses Remaining: (# of guesses remaining for the user).
+    document.getElementById("guesses-remaining").innerHTML = remainingGuesses;
+};
+// // // As the user guesses the correct letters, reveal them: m a d o _  _ a.
+function guessLetter(letter) {
+    if (remainingGuesses > 0) {
+        if (lettersGuessed.indexOf(letter) === -1) {
+            lettersGuessed.push(letter);
+            fillBlanks(letter);
+        }
+    }
+};
+
 function fillBlanks(letter) {
     var position = [];
     for (var i = 0; i < randomWord.length; i++) {
@@ -42,27 +57,40 @@ function fillBlanks(letter) {
     else {
         for(var i = 0; i < position.length; i++)
         progressWord[position[i]] = letter}
-}
+};
+// // // Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
 
-// // Number of Guesses Remaining: (# of guesses remaining for the user).
-//     //make this remainingGuesses minus 1
-document.getElementById("guesses-remaining").innerHTML = remainingGuesses;
+document.onkeyup = function(event) {
+    // Check to make sure a-z was pressed.
+    if(event.keyCode >= 65 && event.keyCode <= 90) {
+        guessLetter(event.key.toLowerCase());
+        startGame();
+        youWin();
+    }
+    if(gameOver) {
+        resetPage();
+        gameOver = false;
+    }};
 
-// // Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+// // After the user wins/loses the win/lose screen appears in the left column,
+function youWin() {
+    if(progressWord.indexOf("_") === -1) {
+        totalWins++;
+        gameOver = true;
+        document.getElementById("winImage").style.display = "block";
+    }
+};
+function youLose() {
+    if(remainingGuesses <= 0) {
+        totalWins++;
+        gameOver = true;
+        document.getElementById("loseImage").style.display = "block";
 
-var lettersGuessed = event.key;
+    }
+};
 
-
-
-
-// After the user wins/loses the win screen appears in the left column,
-document.getElementById("wins-total").innerHTML = totalWins;
-
-
-//  then the game should automatically choose another word and make the user play it.
-
-
-
-
-
+// //  then the game should automatically choose another word and make the user play it.
+// ***this sort-of works; if you press a letter, the game restarts, but I'm out of time to figure out how to make the 
+// image re-hide upon reset and how to make the letter you press NOT be your first guess.  Also need to figure out
+// why EVERY letter I guess ends up in the last div and not just the wrong ones.
 
